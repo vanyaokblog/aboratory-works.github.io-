4746ce40168322ee51c5f117bb8ce35bdfadf029
vanyaokblog <114875538+vanyaokblog@users.noreply.github.com>
2022-11-01 14:20:00 +0000

    Add files via upload

---
+++ LR-1 (вторая часть)/LR_1_3/LR_1_3/LR_1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LR_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            num1 = int.Parse(System.Console.ReadLine());
+            num2 = int.Parse(System.Console.ReadLine());
+
+            (num1, num2) = (num2, num1);
+
+            System.Console.WriteLine($"{num1}, {num2}");
+        }
+    }
+}
---
+++ LR-1 (вторая часть)/LR_1_4/LR_1_4/LR_1_4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LR_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname;
+            int age;
+            
+            name = System.Console.ReadLine();
+            surname = System.Console.ReadLine();
+            age = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine($"{name}, {surname}, {age}");
+        }
+    }
+}
