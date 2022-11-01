1e9edfea8912b44259836a1692556a0565214588
vanyaokblog <114875538+vanyaokblog@users.noreply.github.com>
2022-11-01 14:19:03 +0000

    Add files via upload

---
+++ LR-1/LR_1_1/LR_1_1/LR_1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LR_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            num1 = int.Parse(System.Console.ReadLine());
+            num2 = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine(num1+num2);
+        }
+    }
+}
---
+++ LR-1/LR_1_2/LR_1_2/LR_1_2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LR_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            bool flag;
+
+            x = int.Parse(System.Console.ReadLine());
+            y = int.Parse(System.Console.ReadLine());
+            z = int.Parse(System.Console.ReadLine());
+
+            flag = (x > z);
+            System.Console.WriteLine(flag);
+
+            flag = (x+5 > y);
+            System.Console.WriteLine(flag);
+
+            flag = ((z > x) || (y > x));
+            System.Console.WriteLine(flag);
+        }
+    }
+}
