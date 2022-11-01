d60f5ada33765fe533776d6e8b85062014a0ada9
vanyaokblog <114875538+vanyaokblog@users.noreply.github.com>
2022-11-01 14:09:09 +0000

    Add files via upload

---
+++ LR-4/LR_4_2_1/LR_4_2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LR_4_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {135, 134, 130, 120, 33, 3, 9, 34 };
+            int max_n;
+
+            max_n = -1;
+
+            foreach (int num in array)
+            {
+                if (num > max_n) { 
+                    max_n = num;
+                }
+            }
+
+            foreach (int num in array)
+            {
+                System.Console.WriteLine($"{num} / {max_n} = {num/max_n}");
+            }
+
+        }
+    }
+}
---
+++ LR-4/LR_4_2_2/LR_4_2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LR_4_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {135, 134, -130, 120, -33, -3, -9, 34 };
+            int count;
+
+            count = 0;
+
+            foreach (int num in array)
+            {
+                if (num > 0)
+                {
+                    count += 1;
+                }
+            }
+
+            System.Console.WriteLine($"Положительных чисел: {count}");
+        }
+    }
+}
