2d039efcf08e00882952728368583affffbec16f
vanyaokblog <114875538+vanyaokblog@users.noreply.github.com>
2022-11-01 14:11:25 +0000

    Add files via upload

---
+++ LR-5/LR_5_1_1/LR_5_1_1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LR_5_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, mult = 1;
+
+            System.Console.WriteLine("Введите кол-во строк: ");
+            int n = int.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("Введите кол-во столбцов: ");
+            int m = int.Parse(System.Console.ReadLine());
+
+            int[,] array = new int[n, m];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    array[i, x] = int.Parse(System.Console.ReadLine());
+                    System.Console.WriteLine($"Строк: {i+1}, столбцов: {x+1}");
+                }
+            }
+
+            System.Console.WriteLine();
+
+            foreach (int i in array)
+            {
+                sum += 1;
+            }
+
+            System.Console.WriteLine($"Сумма всех элементов равна {sum}");
+            System.Console.WriteLine();
+
+            foreach (int i in array)
+            {
+                mult *= i;
+            }
+
+            System.Console.WriteLine($"Произведение всех элементов равно {mult}");
+        }
+    }
+}
