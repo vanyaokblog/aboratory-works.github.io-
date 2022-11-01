184719fa7c674a30e954ce0e6b0f21bec487941c
vanyaokblog <114875538+vanyaokblog@users.noreply.github.com>
2022-11-01 14:05:55 +0000

    Add files via upload

---
+++ LR-2/LR_2_1_4/LR_2_1_4/LR_2_1_4/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LR_2_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = int.Parse(System.Console.ReadLine());
+            b = int.Parse(System.Console.ReadLine());
+            c = int.Parse(System.Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                if (Math.Pow(a,2) == Math.Pow(b,2) + Math.Pow(c, 2))
+                {
+                    System.Console.WriteLine("Треугольник является прямоугольным");
+                }
+                else
+                {
+                    System.Console.WriteLine("Треугольник не является прямоугольным");
+                }
+            }
+
+            if (b > c && b > a)
+            {
+                if (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2))
+                {
+                    System.Console.WriteLine("Треугольник является прямоугольным");
+                }
+                else
+                {
+                    System.Console.WriteLine("Треугольник не является прямоугольным");
+                }
+            }
+
+            if (c > b && c > a)
+            {
+                if (Math.Pow(c, 2) == Math.Pow(b, 2) + Math.Pow(a, 2))
+                {
+                    System.Console.WriteLine("Треугольник является прямоугольным");
+                }
+                else
+                {
+                    System.Console.WriteLine("Треугольник не является прямоугольным");
+                }
+            }
+        }
+    }
+}
---
+++ LR-2/LR_2_3_4/LR_2_3_4/LR_2_3_4/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace LR_2_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum, sum2;
+            string num_str;
+
+            sum = 0;
+            sum2 = 0;
+
+            System.Console.WriteLine("Введите шестизначное число:");
+
+            num_str = System.Console.ReadLine();
+            num = int.Parse(num_str);
+
+            if (num_str.Length < 6)
+            {
+                System.Console.WriteLine("Число не шестизначное");
+            }
+
+            else
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    if (i >= 3)
+                    {
+                        sum2 = sum2 + int.Parse(num_str[i].ToString());
+
+                    }
+                    else
+                    {
+                        sum = sum + int.Parse(num_str[i].ToString());
+                    }
+                }
+
+                if (sum == sum2)
+                {
+                    System.Console.WriteLine("Число является счастливым");
+                }
+                else
+                {
+                    System.Console.WriteLine("Число является не счастливым");
+                }
+            }
+        }
+    }
+}
